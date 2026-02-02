af6b62e60489ea9bf56420bfc4aeefd8e0cec23d
AhmadAS <ahmad.mansi.cs@gmail.com>
2026-02-02 15:02:53 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    // For GitHub Pages with a custom domain (CNAME), keep base as '/'
+    // If you deploy to https://<user>.github.io/<repo>/ instead, set base: '/Tamr-Yafa/'
+    base: '/',
     plugins: [
         react({
             babel: {
